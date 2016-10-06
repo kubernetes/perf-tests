9bec71e1ebc21a476e91ae97b3e7295b6bbb1a8d
gmarek <gmarek@google.com>
2016-10-06 15:26:19 +0200

    and again

--- verify/verify-boilerplate.sh
+++ verify/verify-boilerplate.sh
@@ -19,7 +19,7 @@ set -o nounset
 set -o pipefail
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
-boiler="${KUBE_ROOT}/hack/boilerplate/boilerplate.py"
+boiler="${KUBE_ROOT}/verify/boilerplate/boilerplate.py"
 
 files_need_boilerplate=($(${boiler} "$@"))
 

1dc14085aed3a0ba7f99bde5806dcc7adbf3c92e
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2018-03-29 14:17:44 +1100

    Fixed identation, added requirements.txt and other small changes

--- dns/py/data.py
+++ dns/py/data.py
@@ -15,9 +15,10 @@
 # limitations under the License.
 
 import logging
-import numpy
-import re
 import sqlite3
+import re
+from functools import reduce
+import numpy
 
 from params import PARAMETERS
 
@@ -25,26 +26,23 @@ _log = logging.getLogger(__name__)
 
 
 class Result(object):
-  """
+    """
   Represents a column in the results table.
   """
-  def __init__(self, name, val_type, regex):
-    self.name = name
-    self.val_type = val_type
-    self.regex = regex
+
+    def __init__(self, name, val_type, regex):
+        self.name = name
+        self.val_type = val_type
+        self.regex = regex
+
 
 RESULTS = [
-    Result('queries_sent', int,
-           re.compile(r'\s*Queries sent:\s*(\d+)')),
+    Result('queries_sent', int, re.compile(r'\s*Queries sent:\s*(\d+)')),
     Result('queries_completed', int,
            re.compile(r'\s*Queries completed:\s*(\d+).*')),
-    Result('queries_lost', int,
-           re.compile(r'\s*Queries lost:\s*(\d+).*')),
-    Result('run_time', float,
-           re.compile(r'\s*Run time \(s\):\s*([0-9.]+)')),
-    Result('qps', float,
-           re.compile(r'\s*Queries per second:\s*([0-9.]+)')),
-
+    Result('queries_lost', int, re.compile(r'\s*Queries lost:\s*(\d+).*')),
+    Result('run_time', float, re.compile(r'\s*Run time \(s\):\s*([0-9.]+)')),
+    Result('qps', float, re.compile(r'\s*Queries per second:\s*([0-9.]+)')),
     Result('avg_latency', float,
            re.compile(r'\s*Average Latency \(s\):\s*([0-9.]+).*')),
     Result('min_latency', float,
@@ -62,80 +60,79 @@ RESULTS = [
 
 
 class Parser(object):
-  """
+    """
   Parses dnsperf output file.
   """
-  def __init__(self, out):
-    self.lines = [x.strip() for x in out.split('\n')]
-    self.results = {}
-    self.histogram = []
 
-  def parse(self):
-    self._parse_results()
-    self._parse_histogram()
-    self._compute_derived()
+    def __init__(self, out):
+        self.lines = [x.strip() for x in out.split('\n')]
+        self.results = {}
+        self.histogram = []
 
-  def _parse_results(self):
-    results = {}
-    for line in self.lines:
-      for result in RESULTS:
-        if result.regex is None:
-          continue
-        match = result.regex.match(line)
-        if not match:
-          continue
-        results[result.name] = result.val_type(match.group(1))
-    self.results = results
+    def parse(self):
+        self._parse_results()
+        self._parse_histogram()
+        self._compute_derived()
 
-  def _parse_histogram(self):
-    lines = [x for x in self.lines if re.match('^#histogram .*', x)]
-    for line in lines:
-      match = re.match(r'^#histogram\s+(\d+) (\d+)', line)
-      rtt, count = [int(x) for x in match.groups()]
-      self.histogram.append([rtt, count])
+    def _parse_results(self):
+        results = {}
+        for line in self.lines:
+            for result in RESULTS:
+                if result.regex is None:
+                    continue
+                match = result.regex.match(line)
+                if not match:
+                    continue
+                results[result.name] = result.val_type(match.group(1))
+        self.results = results
 
-  def _compute_derived(self):
-    # Note: not very efficient, but functional
-    histogram = reduce(
-        list.__add__,
-        [[rtt]*count for rtt, count in self.histogram],
-        [])
-    _log.debug('Latency histogram = %s', histogram)
+    def _parse_histogram(self):
+        lines = [x for x in self.lines if re.match('^#histogram .*', x)]
+        for line in lines:
+            match = re.match(r'^#histogram\s+(\d+) (\d+)', line)
+            rtt, count = [int(x) for x in match.groups()]
+            self.histogram.append([rtt, count])
 
-    for name, ptile in [('latency_50_percentile', 50),
-                        ('latency_95_percentile', 95),
-                        ('latency_99_percentile', 99),
-                        ('latency_99_5_percentile', 99.5)]:
-      self.results[name] = float(numpy.percentile(histogram, ptile)) # pylint: disable=no-member
+    def _compute_derived(self):
+        # Note: not very efficient, but functional
+        histogram = reduce(
+            list.__add__, [[rtt] * count for rtt, count in self.histogram], [])
+        _log.debug('Latency histogram = %s', histogram)
+
+        for name, ptile in [('latency_50_percentile',
+                             50), ('latency_95_percentile',
+                                   95), ('latency_99_percentile', 99),
+                            ('latency_99_5_percentile', 99.5)]:
+            self.results[name] = float(numpy.percentile(histogram, ptile))  # pylint: disable=no-member
 
 
 class ResultDb(object):
-  def __init__(self, dbfile):
-    self.db = sqlite3.connect(dbfile)
+    def __init__(self, dbfile):
+        self.db = sqlite3.connect(dbfile)
 
-    self.c = self.db.cursor()
+        self.c = self.db.cursor()
 
-    sql = """-- run parameters
+        sql = """-- run parameters
 CREATE TABLE IF NOT EXISTS runs (
   run_id,
   run_subid,
   {params},
   primary key (run_id, run_subid)
 )""".format(params=',\n  '.join([param.name for param in PARAMETERS]))
-    self.c.execute(sql)
-    _log.debug('%s', sql)
+        self.c.execute(sql)
+        _log.debug('%s', sql)
 
-    sql = """-- run results
+        sql = """-- run results
 CREATE TABLE IF NOT EXISTS results (
   run_id,
   run_subid,
   {results},
   primary key (run_id, run_subid)
 )""".format(results=',\n  '.join([r.name for r in RESULTS]))
-    _log.debug('%s', sql)
-    self.c.execute(sql)
+        _log.debug('%s', sql)
+        self.c.execute(sql)
 
-    sql = """-- latency histogram
+        sql = """-- latency histogram
 CREATE TABLE IF NOT EXISTS histograms (
   run_id,
   run_subid,
@@ -143,66 +140,63 @@ CREATE TABLE IF NOT EXISTS histograms (
   rtt_ms_count
 )
 """
-    _log.debug('%s', sql)
-    self.c.execute(sql)
+        _log.debug('%s', sql)
+        self.c.execute(sql)
 
-    _log.info('Using DB %s', dbfile)
+        _log.info('Using DB %s', dbfile)
 
-  def put(self, results, ignore_if_dup=True):
-    key = [results['params']['run_id'], results['params']['run_subid']]
-    if self._exists(key) and ignore_if_dup:
-      _log.info('Ignoring duplicate results %s', key)
-      return
+    def put(self, results, ignore_if_dup=True):
+        key = [results['params']['run_id'], results['params']['run_subid']]
+        if self._exists(key) and ignore_if_dup:
+            _log.info('Ignoring duplicate results %s', key)
+            return
 
-    sql = ('INSERT INTO runs (run_id, run_subid, '
-           + ','.join([p.name for p in PARAMETERS])
-           + ') VALUES ('
-           + ','.join(['?'] * (2 + len(PARAMETERS)))
-           + ')')
-    _log.debug('runs sql -- %s', sql)
-    self.c.execute(sql, key + [
-        results['params'][p.name] if p.name in results['params'] else None
-        for p in PARAMETERS
-    ])
+        sql = ('INSERT INTO runs (run_id, run_subid, ' +
+               ','.join([p.name for p in PARAMETERS]) + ') VALUES (' +
+               ','.join(['?'] * (2 + len(PARAMETERS))) + ')')
+        _log.debug('runs sql -- %s', sql)
+        self.c.execute(sql, key + [
+            results['params'][p.name] if p.name in results['params'] else None
+            for p in PARAMETERS
+        ])
 
-    sql = ('INSERT INTO results (run_id, run_subid, '
-           + ','.join([r.name for r in RESULTS])
-           + ') VALUES ('
-           + ','.join(['?'] * (2 + len(RESULTS)))
-           + ')')
-    _log.debug('results sql -- %s', sql)
-    self.c.execute(sql, key +
-                   [results['data'][r.name]
-                       if r.name in results['data'] else None
-                    for r in RESULTS])
+        sql = ('INSERT INTO results (run_id, run_subid, ' +
+               ','.join([r.name for r in RESULTS]) + ') VALUES (' +
+               ','.join(['?'] * (2 + len(RESULTS))) + ')')
+        _log.debug('results sql -- %s', sql)
+        self.c.execute(sql, key + [
+            results['data'][r.name] if r.name in results['data'] else None
+            for r in RESULTS
+        ])
 
-    for rtt_ms, count in results['data']['histogram']:
-      data = {
-          'run_id': results['params']['run_id'],
-          'run_subid': results['params']['run_subid'],
-          'rtt_ms': rtt_ms,
-          'rtt_ms_count': count,
-      }
+        for rtt_ms, count in results['data']['histogram']:
+            data = {
+                'run_id': results['params']['run_id'],
+                'run_subid': results['params']['run_subid'],
+                'rtt_ms': rtt_ms,
+                'rtt_ms_count': count,
+            }
 
-      columns = ','.join(data.keys())
-      qs = ','.join(['?'] * len(data))
-      stmt = 'INSERT INTO histograms (' + columns + ') VALUES (' + qs + ')'
-      _log.debug('histogram sql -- %s', stmt)
-      self.c.execute(stmt, data.values())
+            columns = ','.join(data.keys())
+            qs = ','.join(['?'] * len(data))
+            stmt = 'INSERT INTO histograms (' + columns + ')VALUES(' + qs + ')'
+            _log.debug('histogram sql -- %s', stmt)
+            self.c.execute(stmt, data.values())
 
-  def get_results(self, run_id, run_subid):
-    sql = ('SELECT ' + ','.join([r.name for r in RESULTS])
-           + ' FROM results WHERE run_id = ? and run_subid = ?')
-    _log.debug('%s', sql)
-    self.c.execute(sql, (run_id, run_subid))
-    rows = self.c.fetchall()
-    return dict(zip([r.name for r in RESULTS], rows[0])) if rows else None
+    def get_results(self, run_id, run_subid):
+        sql = ('SELECT ' + ','.join([r.name for r in RESULTS]) +
+               ' FROM results WHERE run_id = ? and run_subid = ?')
+        _log.debug('%s', sql)
+        self.c.execute(sql, (run_id, run_subid))
+        rows = self.c.fetchall()
+        return dict(zip([r.name for r in RESULTS], rows[0])) if rows else None
 
-  def _exists(self, key):
-    self.c.execute(
-        'SELECT COUNT(*) FROM runs WHERE run_id = ? and run_subid = ?', key)
-    count = self.c.fetchall()[0][0]
-    return count != 0
+    def _exists(self, key):
+        self.c.execute(
+            'SELECT COUNT(*) FROM runs WHERE run_id = ? and run_subid = ?',
+            key)
+        count = self.c.fetchall()[0][0]
+        return count != 0
 
-  def commit(self):
-    self.db.commit()
+    def commit(self):
+        self.db.commit()
--- dns/py/data_test.py
+++ dns/py/data_test.py
@@ -21,24 +21,23 @@ from data import Parser, ResultDb
 
 
 class DataTest(unittest.TestCase):
-  def setUp(self):
-    self.db = ResultDb(':memory:')
+    def setUp(self):
+        self.db = ResultDb(':memory:')
 
-  def test_parser(self):
-    raw = open('fixtures/raw.txt').read()
-    parser = Parser(raw)
-    parser.parse()
-    # Check a subset of fields
-    self.assertEquals(1000, parser.results['queries_sent'])
+    def test_parser(self):
+        raw = open('fixtures/raw.txt').read()
+        parser = Parser(raw)
+        parser.parse()
+        # Check a subset of fields
+        self.assertEqual(1000, parser.results['queries_sent'])
 
-  def test_result_db(self):
-    results = yaml.load(open('fixtures/results.yaml'))
+    def test_result_db(self):
+        results = yaml.load(open('fixtures/results.yaml'))
 
-    self.db.put(results)
-    res = self.db.get_results(1234, 0)
-    self.assertEquals(727354, res['queries_sent'])
-    self.db.put(results) # dup
+        self.db.put(results)
+        res = self.db.get_results(1234, 0)
+        self.assertEqual(727354, res['queries_sent'])
+        self.db.put(results)  # dup
 
-    self.assertRaises(
-        Exception,
-        lambda: self.db.put(results, ignore_if_dup=False))
+        self.assertRaises(Exception,
+                          lambda: self.db.put(results, ignore_if_dup=False))

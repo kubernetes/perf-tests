421d348a4f737e385105724d0dbe58f5e813c903
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2018-04-05 12:00:44 +1000

    Improvements on report queries

--- dns/report_queries/99th_percentile_latency_with_cache.sql
+++
@@ -1,3 +0,0 @@
-SELECT max(latency_99_percentile) as max_latency_with_cache_99th_percentile
-FROM results NATURAL JOIN runs
-WHERE dnsmasq_cache IS NOT NULL;
--- dns/report_queries/99th_percentile_latency_without_cache.sql
+++
@@ -1,3 +0,0 @@
-SELECT max(latency_99_percentile) as max_latency_without_cache_99th_percentile
-FROM results NATURAL JOIN runs
-WHERE dnsmasq_cache IS NULL;

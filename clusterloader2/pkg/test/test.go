0998cf6e88e80c2b1da11f15296616acf482c523
Krzysztof Siedlecki <ksiedlecki@google.com>
2018-07-16 19:17:47 +0200

    adding test executor interface

--- clusterloader2/pkg/test/test.go
+++ clusterloader2/pkg/test/test.go
@@ -21,6 +21,17 @@ import (
 
 	"k8s.io/perf-tests/clusterloader2/api"
 	"k8s.io/perf-tests/clusterloader2/pkg/framework"
+	"k8s.io/perf-tests/clusterloader2/pkg/state"
+)
+
+var (
+	// CreateContext global function for creating context.
+	// This function should be set by Context implementation.
+	CreateContext CreatContextFunc
+
+	// Test is a singleton for test execution object.
+	// This object should be set by TestExecutor implementation.
+	Test TestExecutor
 )
 
 // RunTest runs test based on provided test configuration.
@@ -31,10 +42,13 @@ func RunTest(f *framework.Framework, conf *api.Config) error {
 	if conf == nil {
 		return fmt.Errorf("test config must be provided")
 	}
-	err := f.CreateAutomanagedNamespaces(int(conf.AutomanagedNamespaces))
-	defer f.DeleteAutomanagedNamespaces()
-	if err != nil {
-		return fmt.Errorf("automanaged namespaces creation failed: %v", err)
+	if CreateContext == nil {
+		return fmt.Errorf("no CreateContext function installed")
 	}
-	return nil
+	if Test == nil {
+		return fmt.Errorf("no Test installed")
+	}
+
+	ctx := CreateContext(f, state.NewNamespacesState())
+	return Test.ExecuteTest(ctx, conf)
 }

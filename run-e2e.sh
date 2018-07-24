f9666e8455ad57f0444576b75a494fc7e875e652
i076657 <ajaysimha.durbhakula@sap.com>
2018-07-24 12:30:38 +0530

    Adding Flags to run ClusterLoader,Netperf and DNS

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# run the script to run e2e tests for cluster loader, netperf and dns. 
+# This enables the same to be invoked from kubetest from test-infra.
+
 set -o nounset
 set -o pipefail
 
@@ -26,7 +44,8 @@ do
                --core-dns )
                #CORE-DNS
                        cd ${PERFTEST_ROOT}/dns
-                       ./run --dns-server coredns --params params/coredns/default.yaml --out-dir out
+		       mkdir out
+                       ./run --dns-server coredns --params params/coredns/default.yaml --out-dir out --use-cluster-dns
                        exit
                        ;;
                --help | -h )

481b7e8fb58927c123fe3307c63a4de3fc36dca6
Orsolya Kiss <kissorsolya17@gmail.com>
2018-10-19 13:26:27 +0000

    Fix gofmt error in launch.go imports

--- network/benchmarks/netperf/launch.go
+++ network/benchmarks/netperf/launch.go
@@ -33,10 +33,10 @@ import (
 	"strings"
 	"time"
 
-	"k8s.io/client-go/kubernetes"
 	api "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 

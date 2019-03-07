34b3a88b4565327e379827422a492ec4db72374b
Jonathan Basseri <misterikkit@google.com>
2019-03-06 18:23:09 -0800

    Add deep links to PerfDash.
    This change adds two-way binding between the drop-down menus of PerfDash
    and the set of URL query parameters. The result is that changing the
    drop-downs will change the URL, and changing the URL will update the UI.
    
    More importantly, these URLs can be shared, causing PerfDash to load a
    specific graph.

--- perfdash/www/script.js
+++ perfdash/www/script.js
@@ -14,15 +14,20 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-var app = angular.module('PerfDashApp', ['ngMaterial', 'chart.js']);
+var app = angular.module('PerfDashApp', ['ngMaterial', 'ngRoute', 'chart.js']);
 
-var PerfDashApp = function(http, scope) {
+var PerfDashApp = function(http, scope, route) {
     this.http = http;
     this.scope = scope;
+    this.route = route;
+    this.jobNames = [];
+    this.metricCategoryNames = [];
     this.metricNames = [];
+    this.selectedLabels = {};
     this.onClick = this.onClickInternal_.bind(this);
     this.cap = 0;
     this.currentCall = 0;
+    this.scope.$on('$routeChangeSuccess', this.routeChanged.bind(this));
 };
 
 
@@ -57,12 +62,43 @@ PerfDashApp.prototype.refresh = function() {
     });
 };
 
+// Update the select drop-downs based on the query params.
+PerfDashApp.prototype.routeChanged = function(event, data) {
+    var app = this;
+    angular.forEach(this.route.current.params, function(value, name) {
+        switch (name) {
+            case "jobname":
+                if (app.jobName !== value) {
+                    app.jobName = value;
+                    app.jobNameChanged();
+                }
+                break;
+            case "metriccategoryname":
+                if (app.metricCategoryName !== value) {
+                    app.metricCategoryName = value;
+                    app.metricCategoryNameChanged();
+                }
+                break;
+            case "metricname":
+                if (app.metricName !== value) {
+                    app.metricName = value;
+                    app.metricNameChanged();
+                }
+                break;
+            default:
+                app.selectedLabels[name] = value;
+        }
+    });
+    this.labelChanged();
+}
+
 // Update the data to graph, using the selected jobName
 PerfDashApp.prototype.jobNameChanged = function() {
+    this.setURLParameters();
     this.http.get("metriccategorynames", {params: {jobname: this.jobName}})
             .success(function(data) {
                     this.metricCategoryNames = data;
-                    if (this.metricCategoryNames == undefined ||  this.metricCategoryNames.indexOf(this.metricCategoryName) == -1) {
+                    if (this.metricCategoryName == undefined || this.metricCategoryNames.indexOf(this.metricCategoryName) == -1) {
                          this.metricCategoryName = this.metricCategoryNames[0]
                     }
                     this.metricCategoryNameChanged();
@@ -75,6 +111,7 @@ PerfDashApp.prototype.jobNameChanged = function() {
 
 // Update the data to graph, using the selected jobName
 PerfDashApp.prototype.metricCategoryNameChanged = function() {
+    this.setURLParameters();
     this.http.get("metricnames", {params: {jobname: this.jobName, metriccategoryname: this.metricCategoryName}})
             .success(function(data) {
                     this.metricNames = data;
@@ -91,6 +128,7 @@ PerfDashApp.prototype.metricCategoryNameChanged = function() {
 
 // Update the data to graph, using the selected metricName
 PerfDashApp.prototype.metricNameChanged = function() {
+    this.setURLParameters();
     var callId = ++this.currentCall;
     this.http.get("buildsdata", {params: {jobname: this.jobName, metriccategoryname: this.metricCategoryName, metricname: this.metricName}})
             .success(function(data) {
@@ -111,6 +149,7 @@ PerfDashApp.prototype.metricNameChanged = function() {
 
 // Update the data to graph, using selected labels
 PerfDashApp.prototype.labelChanged = function() {
+    this.setURLParameters();
     this.seriesData = [];
     this.series = [];
     result = this.getData(this.selectedLabels);
@@ -140,12 +179,29 @@ PerfDashApp.prototype.labelChanged = function() {
     this.cap = 0;
 };
 
+// Overwrite the URL query params with the current drop-down selections.
+PerfDashApp.prototype.setURLParameters = function() {
+    var newParams = {};
+    // By setting all existing params to null in newParams, we will delete label
+    // parameters that do not apply to the current selection.
+    angular.forEach(this.route.current.params, function(ignore, name) {
+        newParams[name] = null;
+    });
+    newParams["jobname"] = this.jobName;
+    newParams["metriccategoryname"] = this.metricCategoryName;
+    newParams["metricname"] = this.metricName;
+    angular.forEach(this.selectedLabels, function(value, name) {
+        newParams[name] = value;
+    });
+    this.route.updateParams(newParams);
+}
+
 // Get all of the builds for the data set (e.g. build numbers)
 PerfDashApp.prototype.getBuilds = function() {
     return Object.keys(this.data)
 };
 
-// Verify if selected labels are in label set 
+// Verify if selected labels are in label set.
 function verifySelectedLabels(selectedLabels, allLabels) {
     if (selectedLabels == undefined || allLabels == undefined) {
         return false;
@@ -185,6 +241,7 @@ function isEmptySet(obj) {
 
 // Get the set of all labels (e.g. 'resources', 'verbs') in the data set
 PerfDashApp.prototype.getLabels = function() {
+    // Set is a map of every label name to a set of every label value.
     var set = {};
     angular.forEach(this.data, function(items, build) {
         angular.forEach(items, function(item) {
@@ -266,10 +323,15 @@ PerfDashApp.prototype.getStream = function(data, stream) {
     return result;
 };
 
-app.controller('AppCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
-    $scope.controller = new PerfDashApp($http, $scope);
+app.controller('AppCtrl', ['$scope', '$http', '$interval', '$route', function($scope, $http, $interval, $route) {
+    $scope.controller = new PerfDashApp($http, $scope, $route);
     $scope.controller.refresh();
 
     // Refresh every 10 min.  The data only refreshes every 10 minutes on the server
     $interval($scope.controller.refresh.bind($scope.controller), 600000)
 }]);
+
+// Add a dummy route so that we can manipulate URL params.
+app.config(function($routeProvider) {
+    $routeProvider.when('/', {})
+});

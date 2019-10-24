6ba68c3638f692db0c3f29152f238bd1e8fe00ca
Jakub Pierewoj <pierewoj@google.com>
2019-10-24 17:39:31 +0200

    Enable restart count validation in clusterloader
    In this change, restart count validation is implemented. Note that it
    will only affect tests, which have this feature enabled (by using
    enable_restart_count_check.yaml override).
    
    Testing:
    * ran kubemark test locally using clusterloader, cluster running on GCE

--- clusterloader2/pkg/measurement/common/system_pod_metrics_test.go
+++ clusterloader2/pkg/measurement/common/system_pod_metrics_test.go
@@ -19,6 +19,9 @@ package common
 import (
 	"reflect"
 	"testing"
+
+	"gopkg.in/yaml.v2"
+	"k8s.io/perf-tests/clusterloader2/pkg/measurement"
 )
 
 func Test_subtractInitialRestartCounts(t *testing.T) {
@@ -57,6 +60,63 @@ func Test_subtractInitialRestartCounts(t *testing.T) {
 	}
 }
 
+func Test_validateRestartCounts(t *testing.T) {
+	tests := []struct {
+		name    string
+		metrics *systemPodsMetrics
+		config  *measurement.MeasurementConfig
+		wantErr bool
+	}{
+		{
+			name:    "check-disabled",
+			metrics: generatePodMetrics("p", "c", 1),
+			config:  buildConfig(t, false, nil),
+			wantErr: false,
+		},
+		{
+			name:    "check-enabled-violation",
+			metrics: generatePodMetrics("p", "c", 1),
+			config:  buildConfig(t, true, nil),
+			wantErr: true,
+		},
+		{
+			name:    "check-enabled-ok",
+			metrics: generatePodMetrics("p", "c", 0),
+			config:  buildConfig(t, true, nil),
+			wantErr: false,
+		},
+		{
+			name:    "override-equal-to-actual-count",
+			metrics: generatePodMetrics("p", "c", 3),
+			config:  buildConfig(t, true, map[string]int{"c": 3}),
+			wantErr: false,
+		},
+		{
+			name:    "override-below-actual-count",
+			metrics: generatePodMetrics("p", "c", 3),
+			config:  buildConfig(t, true, map[string]int{"c": 2}),
+			wantErr: true,
+		},
+		{
+			name:    "override-for-different-container",
+			metrics: generatePodMetrics("p", "c1", 3),
+			config:  buildConfig(t, true, map[string]int{"c2": 4}),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			overrides, err := getThresholdOverrides(tt.config)
+			if err != nil {
+				t.Fatalf("getThresholdOverrides() error = %v", err)
+			}
+			if err := validateRestartCounts(tt.metrics, tt.config, overrides); (err != nil) != tt.wantErr {
+				t.Errorf("verifyViolations() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
 func generatePodMetrics(podName string, contName string, restartCount int32) *systemPodsMetrics {
 	return &systemPodsMetrics{
 		Pods: []podMetrics{
@@ -68,6 +128,19 @@ func generatePodMetrics(podName string, contName string, restartCount int32) *sy
 						RestartCount: restartCount,
 					},
 				}},
+		},
+	}
+}
+
+func buildConfig(t *testing.T, checkEnabled bool, thresholdOverrides map[string]int) *measurement.MeasurementConfig {
+	serializedOverrides, err := yaml.Marshal(thresholdOverrides)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &measurement.MeasurementConfig{
+		Params: map[string]interface{}{
+			"enableRestartCountCheck":        checkEnabled,
+			"restartCountThresholdOverrides": string(serializedOverrides),
 		},
 	}
 }

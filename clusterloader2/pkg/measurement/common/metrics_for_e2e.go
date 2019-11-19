a177d021d39d581660209bd0c25658d492a1ac4f
sonyafenge <sonyafenge@gmail.com>
2019-11-19 00:58:34 +0000

    Remove interestingControllerManagerMetricsLabels  Changes to be committed: 	modified:   clusterloader2/pkg/measurement/common/metrics_for_e2e.go

--- clusterloader2/pkg/measurement/common/metrics_for_e2e.go
+++ clusterloader2/pkg/measurement/common/metrics_for_e2e.go
@@ -37,109 +37,6 @@ var interestingApiServerMetricsLabels = []string{
 	"etcd_request_latencies_summary",
 }
 
-var interestingControllerManagerMetricsLabels = []string{
-	//remove all outdated metrics labels and replace with new labels
-
-	//	"apiserver_audit_event_total",
-	//	"apiserver_audit_requests_rejected_total",
-	//	"apiserver_client_certificate_expiration_seconds_bucket",
-	//	"apiserver_client_certificate_expiration_seconds_count",
-	//	"apiserver_client_certificate_expiration_seconds_sum",
-	//	"apiserver_storage_data_key_generation_duration_seconds_bucket",
-	//	"apiserver_storage_data_key_generation_duration_seconds_count",
-	//	"apiserver_storage_data_key_generation_duration_seconds_sum",
-	//	"apiserver_storage_data_key_generation_failures_total",
-	//	"apiserver_storage_data_key_generation_latencies_microseconds_bucket",
-	//	"apiserver_storage_data_key_generation_latencies_microseconds_count",
-	//	"apiserver_storage_data_key_generation_latencies_microseconds_sum",
-	//	"apiserver_storage_envelope_transformation_cache_misses_total",
-	//	"attachdetach_controller_forced_detaches",
-	//	"authenticated_user_requests",
-	//	"authentication_attempts",
-	//	"authentication_duration_seconds_bucket",
-	//	"authentication_duration_seconds_count",
-	//	"authentication_duration_seconds_sum",
-	//	"cronjob_controller_rate_limiter_use",
-	//	"daemon_controller_rate_limiter_use",
-	//	"deployment_controller_rate_limiter_use",
-	//	"endpoint_controller_rate_limiter_use",
-	//	"gc_controller_rate_limiter_use",
-	//	"get_token_count",
-	//	"get_token_fail_count",
-	"go_gc_duration_seconds",
-	"go_gc_duration_seconds_count",
-	"go_gc_duration_seconds_sum",
-	//	"go_goroutines",
-	//	"go_info",
-	//	"go_memstats_alloc_bytes",
-	//	"go_memstats_alloc_bytes_total",
-	//	"go_memstats_buck_hash_sys_bytes",
-	//	"go_memstats_frees_total",
-	//	"go_memstats_gc_cpu_fraction",
-	//	"go_memstats_gc_sys_bytes",
-	//	"go_memstats_heap_alloc_bytes",
-	//	"go_memstats_heap_idle_bytes",
-	//	"go_memstats_heap_inuse_bytes",
-	//	"go_memstats_heap_objects",
-	//	"go_memstats_heap_released_bytes",
-	//	"go_memstats_heap_sys_bytes",
-	//	"go_memstats_last_gc_time_seconds",
-	//	"go_memstats_lookups_total",
-	//	"go_memstats_mallocs_total",
-	//	"go_memstats_mcache_inuse_bytes",
-	//	"go_memstats_mcache_sys_bytes",
-	//	"go_memstats_mspan_inuse_bytes",
-	//	"go_memstats_mspan_sys_bytes",
-	//	"go_memstats_next_gc_bytes",
-	//	"go_memstats_other_sys_bytes",
-	//	"go_memstats_stack_inuse_bytes",
-	//	"go_memstats_stack_sys_bytes",
-	//	"go_memstats_sys_bytes",
-	//	"go_threads",
-	//	"job_controller_rate_limiter_use",
-	//	"kubernetes_build_info",
-	//	"leader_election_master_status",
-	//	"namespace_controller_rate_limiter_use",
-	//	"node_collector_evictions_number",
-	//	"node_collector_unhealthy_nodes_in_zone",
-	//	"node_collector_zone_health",
-	//	"node_collector_zone_size",
-	//	"node_ipam_controller_rate_limiter_use",
-	//	"node_lifecycle_controller_rate_limiter_use",
-	//	"persistentvolume_protection_controller_rate_limiter_use",
-	//	"persistentvolumeclaim_protection_controller_rate_limiter_use",
-	"process_cpu_seconds_total",
-	"process_max_fds",
-	"process_open_fds",
-	"process_resident_memory_bytes",
-	"process_start_time_seconds",
-	"process_virtual_memory_bytes",
-	"process_virtual_memory_max_bytes",
-	//	"replication_controller_rate_limiter_use",
-	//	"resource_quota_controller_rate_limiter_use",
-	//	"rest_client_request_duration_seconds_bucket",
-	//	"rest_client_request_duration_seconds_count",
-	"rest_client_request_duration_seconds_sum",
-	//	"rest_client_request_latency_seconds_bucket",
-	//	"rest_client_request_latency_seconds_count",
-	"rest_client_request_latency_seconds_sum",
-	"rest_client_requests_total",
-	//	"service_controller_rate_limiter_use",
-	//	"serviceaccount_controller_rate_limiter_use",
-	//	"serviceaccount_tokens_controller_rate_limiter_use",
-	"workqueue_adds_total",
-	//	"workqueue_depth",
-	"workqueue_longest_running_processor_seconds",
-	//	"workqueue_queue_duration_seconds_bucket",
-	//	"workqueue_queue_duration_seconds_count",
-	"workqueue_queue_duration_seconds_sum",
-	"workqueue_retries_total",
-	"workqueue_unfinished_work_seconds",
-	//	"workqueue_work_duration_seconds_bucket",
-	//	"workqueue_work_duration_seconds_count",
-	"workqueue_work_duration_seconds_sum",
-}
-
 var interestingKubeletMetricsLabels = []string{
 	"kubelet_container_manager_latency_microseconds",
 	"kubelet_docker_errors",
@@ -214,10 +111,6 @@ func filterMetrics(m *metrics.MetricsCollection) {
 	for _, metric := range interestingApiServerMetricsLabels {
 		interestingApiServerMetrics[metric] = (*m).ApiServerMetrics[metric]
 	}
-	interestingControllerManagerMetrics := make(metrics.ControllerManagerMetrics)
-	for _, metric := range interestingControllerManagerMetricsLabels {
-		interestingControllerManagerMetrics[metric] = (*m).ControllerManagerMetrics[metric]
-	}
 	interestingKubeletMetrics := make(map[string]metrics.KubeletMetrics)
 	for kubelet, grabbed := range (*m).KubeletMetrics {
 		interestingKubeletMetrics[kubelet] = make(metrics.KubeletMetrics)
@@ -226,6 +119,5 @@ func filterMetrics(m *metrics.MetricsCollection) {
 		}
 	}
 	(*m).ApiServerMetrics = interestingApiServerMetrics
-	(*m).ControllerManagerMetrics = interestingControllerManagerMetrics
 	(*m).KubeletMetrics = interestingKubeletMetrics
 }

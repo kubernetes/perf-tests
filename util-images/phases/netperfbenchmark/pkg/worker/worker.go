41438407eee684200f7a1b55233add9fa3b3372e
Vivek Thrivikraman <vivek.thrivikraman@est.tech>
2020-10-08 23:15:10 +0530

    change for TCP with iperf and yaml files for clusterloader

--- util-images/phases/netperfbenchmark/pkg/worker/worker.go
+++ util-images/phases/netperfbenchmark/pkg/worker/worker.go
@@ -24,7 +24,8 @@ var listener net.Listener
 var resultCh = make(chan string, 100)
 var resultStatus = make(chan string, 1)
 
-var iperfFn = []string{"", "", "", "Sum", "Sum", "Sum", "Sum", "Sum", "Avg", "Avg", "Min", "Max", "Avg", "Sum"}
+var iperfUDPFn = []string{"", "", "", "Sum", "Sum", "Sum", "Sum", "Sum", "Avg", "Avg", "Min", "Max", "Avg", "Sum"}
+var iperfTCPFn = []string{"Sum", "Avg"}
 
 //WorkerRPC service that exposes ExecTestcase, GetPerfMetrics API for clients
 type WorkerRPC int
@@ -71,19 +72,23 @@ func (w *WorkerRPC) Stop(tc *api.MetricRequest, reply *api.MetricResponse) error
 
 func (w *WorkerRPC) StartTCPClient(tc *api.WorkerRequest, reply *api.WorkerResponse) error {
 	klog.Info("In StartTCPClient")
-	go execCmd(tc.Duration, "iperf3", []string{"-c", tc.DestinationIP, "-f", "K", "-l", "20", "-b", "1M", "-i", "1"})
+	// go execCmd(tc.Duration, "iperf3", []string{"-c", tc.DestinationIP, "-f", "K", "-l", "20", "-b", "1M", "-i", "1"})
+	go execCmd(tc.Duration, "iperf", []string{"-c", tc.DestinationIP, "-f", "K", "-l", "20", "-b", "1M", "-i", "1", "-t", tc.Duration})
 	return nil
 }
 
 func (w *WorkerRPC) StartTCPServer(tc *api.WorkerRequest, reply *api.WorkerResponse) error {
 	klog.Info("In StartTCPServer")
-	go execCmd(tc.Duration, "iperf3", []string{"-s", "-f", "K", "-i", "1"})
+	resultCh <- "TCP"
+	// go execCmd(tc.Duration, "iperf3", []string{"-s", "-f", "K", "-i", "1"})
+	go execCmd(tc.Duration, "iperf", []string{"-s", "-f", "K", "-i", tc.Duration, "-P", tc.NumClients})
 	return nil
 }
 
 func (w *WorkerRPC) StartUDPServer(tc *api.WorkerRequest, reply *api.WorkerResponse) error {
 	//iperf -s -u -e -i <duration> -P <num parallel clients>
 	klog.Info("In StartUDPServer")
+	resultCh <- "UDP"
 	go execCmd(tc.Duration, "iperf", []string{"-s", "-f", "K", "-u", "-e", "-i", tc.Duration, "-P", tc.NumClients})
 	return nil
 }
@@ -114,6 +119,7 @@ func (w *WorkerRPC) StartHTTPServer(tc *api.WorkerRequest, reply *api.WorkerResp
 func (w *WorkerRPC) StartHTTPClient(tc *api.WorkerRequest, reply *api.WorkerResponse) error {
 	//// siege http://localhost:5301/test -d1 -r1 -c1 -t10S
 	//c concurrent r repetitions t time d delay in sec between 1 and d
+	resultCh <- "HTTP"
 	klog.Info("In StartHTTPClient")
 	go execCmd(tc.Duration, "siege",
 		[]string{"http://" + tc.DestinationIP + ":" + api.HttpPort + "/test",
@@ -136,7 +142,6 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 
 func execCmd(duration string, command string, args []string) {
 	cmd := exec.Command(command, args...)
-	resultCh <- command
 	resultCh <- duration
 	out, err := cmd.StdoutPipe()
 	if err != nil {
@@ -189,7 +194,7 @@ func initializeServerRPC() {
 	klog.Info("Started http server")
 
 	//TODO to be removed
-	// test()
+	test()
 }
 
 //TODO to be removed , test method
@@ -200,11 +205,11 @@ func test() {
 		klog.Fatalf("dialing:", err)
 		//TODO WHAT IF FAILS?
 	}
-	podData := &api.WorkerRequest{DestinationIP: "localhost", Duration: "10", NumClients: "2"}
+	podData := &api.WorkerRequest{DestinationIP: "localhost", Duration: "10", NumClients: "1"}
 	var reply api.WorkerResponse
 	metricReq := &api.MetricRequest{}
 	var metricRes api.MetricResponse
-	//TCP TEST
+	// TCP TEST
 	// err = client.Call("WorkerRPC.StartTCPServer", podData, &reply)
 	// time.Sleep(2 * time.Second)
 	// err = client.Call("WorkerRPC.StartTCPClient", podData, &reply)
@@ -212,20 +217,20 @@ func test() {
 	// client.Call("WorkerRPC.Metrics", metricReq, &metricRes)
 	//UDP TEST
 	//resultCh = make(chan string, 140)
-	err = client.Call("WorkerRPC.StartUDPServer", podData, &reply)
-	// time.Sleep(2 * time.Second)
-	err = client.Call("WorkerRPC.StartUDPClient", podData, &reply)
-	err = client.Call("WorkerRPC.StartUDPClient", podData, &reply)
-	time.Sleep(15 * time.Second)
-	client.Call("WorkerRPC.Metrics", metricReq, &metricRes)
-	//HTTP test
-	// // resultCh = make(chan string, 40)
-	// err = client.Call("WorkerRPC.StartHTTPServer", podData, &reply)
+	// err = client.Call("WorkerRPC.StartUDPServer", podData, &reply)
 	// // time.Sleep(2 * time.Second)
-	// err = client.Call("WorkerRPC.StartHTTPClient", podData, &reply)
+	// err = client.Call("WorkerRPC.StartUDPClient", podData, &reply)
+	// err = client.Call("WorkerRPC.StartUDPClient", podData, &reply)
 	// time.Sleep(15 * time.Second)
 	// client.Call("WorkerRPC.Metrics", metricReq, &metricRes)
-	// klog.Info("TESTING COMPLETED!")
+	//HTTP test
+	// // resultCh = make(chan string, 40)
+	err = client.Call("WorkerRPC.StartHTTPServer", podData, &reply)
+	// time.Sleep(2 * time.Second)
+	err = client.Call("WorkerRPC.StartHTTPClient", podData, &reply)
+	time.Sleep(15 * time.Second)
+	client.Call("WorkerRPC.Metrics", metricReq, &metricRes)
+	klog.Info("TESTING COMPLETED!")
 	////////////////////////////////////////////////
 }
 
@@ -243,6 +248,8 @@ func Start() {
 }
 
 func register(port string) {
+	klog.Info("Env variables:", "POD_NAME:"+os.Getenv(api.PodName), " NODE_NAME:"+os.Getenv(api.NodeName),
+		" POD_IP:"+os.Getenv(api.PodIP), " CLUSTER_IP:"+os.Getenv(api.ClusterIp))
 	client, err := rpc.DialHTTP("tcp", api.ControllerHost+":"+port)
 	if err != nil {
 		klog.Error("dialing:", err)
@@ -260,12 +267,12 @@ func register(port string) {
 func parseResult(result []string) ([]float64, error) {
 	klog.Info("Parsing", result[0])
 	switch result[0] {
-	case "iperf3":
-		return parseIperf3(result), nil
-	case "iperf":
-		return parseIperf(result), nil
-	case "siege":
-		return parseSiege(result), nil
+	case "TCP":
+		return parseTCP(result), nil
+	case "UDP":
+		return parseUDP(result), nil
+	case "HTTP":
+		return parseHTTP(result), nil
 	default:
 		return nil, errors.New("wrong result type:" + result[0])
 
@@ -274,46 +281,60 @@ func parseResult(result []string) ([]float64, error) {
 
 }
 
-func parseIperf3(result []string) []float64 {
-	klog.Info("In Parse iperf3")
+func parseTCP(result []string) []float64 {
+	klog.Info("In Parse iperf")
 	dur := result[1]
 	fmtResult := make([][]string, 0)
 	sumResult := make([]float64, 0)
-	unitReg := regexp.MustCompile(`\[\s+|\]\s+|\s+|KBytes\s+|KBytes/sec\s+|sec\s+|pps\s+|ms\s+|/\s+`)
+	unitReg := regexp.MustCompile(`\[\s+|\]\s+|KBytes\s+|KBytes/sec\s*|sec\s+|ms\s+|us\s*`)
+	mulSpaceReg := regexp.MustCompile(`\s+`)
+	cnt := 0
+	sessionId := make(map[string]bool)
 	for _, op := range result {
-		if strings.HasPrefix(op, "-") {
-			klog.Info("TO B DELE:", op)
+		klog.Info(op)
+		if !strings.Contains(op, "0.0-"+dur+".0") {
 			continue
 		}
-		//All Output:
-		fmtResult = append(fmtResult, strings.Split(unitReg.ReplaceAllString(op, " "), " "))
-		split := strings.Split(unitReg.ReplaceAllString(op, " "), " ")
-		if split[1] == "0.00-"+dur+".00" {
+		//TODO try using one regex
+		frmtString := mulSpaceReg.ReplaceAllString(unitReg.ReplaceAllString(op, " "), " ")
+		klog.Info("Trim info:", frmtString)
+		split := strings.Split(frmtString, " ")
+		fmtResult = append(fmtResult, split)
+		//for bug in iperf tcp
+		if len(split) >= 3 && "SUM" != split[1] && split[2] == "0.0-"+dur+".0" { //if the record is for the complete duration of run
+			if _, ok := sessionId[split[1]]; ok {
+				continue
+			}
 			for i, v := range split {
-				if i != 0 && i != 1 {
+				klog.Info("Split", i, ":", v)
+				if i == 1 {
+					sessionId[v] = true
+					continue
+				}
+				if i == 0 || i == 2 || i == 5 { //first index and hte last is ""
 					continue
 				}
 				tmp, err := strconv.ParseFloat(v, 64)
 				if err != nil {
 					klog.Error("conversion error", err)
 				}
-				if len(sumResult) < 11 {
+				if len(sumResult) <= 1 {
 					sumResult = append(sumResult, tmp)
 				} else {
-					switch iperfFn[i] {
+					switch iperfTCPFn[i-3] {
 					case "Sum":
-						sumResult[i-2] = tmp + sumResult[i-2]
+						sumResult[i-3] = tmp + sumResult[i-3]
 					case "Avg":
-						sumResult[i-2] = (tmp + sumResult[i-2]) / (float64(2))
+						sumResult[i-3] = (float64(cnt)*tmp + sumResult[i-3]) / (float64(1 + cnt))
 					case "Min":
-						sumResult[i-2] = math.Min(tmp, sumResult[i-2])
+						sumResult[i-3] = math.Min(tmp, sumResult[i-3])
 					case "Max":
-						sumResult[i-2] = math.Max(tmp, sumResult[i-2])
+						sumResult[i-3] = math.Max(tmp, sumResult[i-3])
 					}
 				}
+				cnt++
 			}
 		}
-		klog.Info("Trimmed:", op)
 	}
 	klog.Info("Matrix:", fmtResult)
 	klog.Info("Final output:", sumResult)
@@ -321,7 +342,7 @@ func parseIperf3(result []string) []float64 {
 
 }
 
-func parseIperf(result []string) []float64 {
+func parseUDP(result []string) []float64 {
 	klog.Info("In Parse iperf")
 	dur := result[1]
 	fmtResult := make([][]string, 0)
@@ -351,7 +372,7 @@ func parseIperf(result []string) []float64 {
 				if len(sumResult) < 11 {
 					sumResult = append(sumResult, tmp)
 				} else {
-					switch iperfFn[i] {
+					switch iperfUDPFn[i] {
 					case "Sum":
 						sumResult[i-3] = tmp + sumResult[i-3]
 					case "Avg":
@@ -371,7 +392,7 @@ func parseIperf(result []string) []float64 {
 	return sumResult
 }
 
-func parseSiege(result []string) []float64 {
+func parseHTTP(result []string) []float64 {
 	canAppend := false
 	sumResult := make([]float64, 0)
 	mulSpaceReg := regexp.MustCompile(`\s+`)

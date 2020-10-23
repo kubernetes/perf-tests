82111d4d56e81f82dcf9d37920ae27fc8a2cef2a
Vivek Thrivikraman <vivek.thrivikraman@est.tech>
2020-10-23 10:57:20 +0530

    New folder for docker image and source code

---
+++ util-images/network/netperfbenchmark/api/types.go
@@ -0,0 +1,137 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package network
+
+import (
+	measurementutil "k8s.io/perf-tests/clusterloader2/pkg/measurement/util"
+)
+
+//net-rpc service listen ports
+const (
+	ControllerRpcSvcPort = "5002"
+	WorkerRpcSvcPort     = "5003"
+	HttpPort             = "5301"
+)
+
+//Environment variables
+const (
+	PodName   = "POD_NAME"
+	NodeName  = "NODE_NAME"
+	PodIP     = "POD_IP"
+	ClusterIp = "CLUSTER_IP"
+)
+
+const (
+	WorkerMode     = "worker"
+	ControllerMode = "controller"
+)
+
+const (
+	Protocol_TCP  = "TCP"
+	Protocol_UDP  = "UDP"
+	Protocol_HTTP = "HTTP"
+)
+
+//TCP result array Index mapping
+const (
+	TCPTransfer = iota
+	TCPBW
+)
+
+//UDP result array Index mapping
+const (
+	UDPTransfer = iota
+	UDPBW
+	UDPJitter
+	UDPLostPkt
+	UDPTotalPkt
+	UDPLatPer
+	UDPLatAvg
+	UDPLatMin
+	UDPLatMax
+	UDPLatStdD
+	UDPPps
+)
+
+//HTTP result array Index mapping
+const (
+	HTTPTxs = iota
+	HTTPAvl
+	HTTPTimeElps
+	HTTPDataTrsfd
+	HTTPRespTime
+	HTTPTxRate
+	HTTPThroughput
+	HTTPConcurrency
+	HTTPTxSuccesful
+	HTTPFailedTxs
+	HTTPLongestTx
+	HTTPShortestTx
+)
+
+const RatioSeparator = ":"
+
+type WorkerPodData struct {
+	PodName    string
+	WorkerNode string
+	PodIp      string
+	ClusterIP  string
+}
+
+type WorkerPodRegReply struct {
+	Response string
+}
+
+type ClientRequest struct {
+	Duration      string
+	Timestamp     int64 //epoch time
+	DestinationIP string
+}
+
+type ServerRequest struct {
+	Duration   string
+	Timestamp  int64 //epoch time
+	NumClients string
+}
+
+type WorkerResponse struct {
+	PodName    string
+	WorkerNode string
+}
+
+type UniquePodPair struct {
+	SrcPodName    string
+	SrcPodIp      string
+	DestPodName   string
+	DestPodIp     string
+	IsLastPodPair bool `default: false`
+}
+
+type MetricRequest struct {
+}
+
+type MetricResponse struct {
+	Result          []float64
+	WorkerStartTime string
+}
+
+type NetworkPerfResp struct {
+	Client_Server_Ratio string
+	Protocol            string
+	Service             string
+	DataItems           []measurementutil.DataItem
+}
---
+++ util-images/network/netperfbenchmark/cmd/main.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"errors"
+	"flag"
+	"strings"
+	"sync"
+
+	"k8s.io/klog"
+	"k8s.io/perf-tests/util-images/phases/netperfbenchmark/api"
+	"k8s.io/perf-tests/util-images/phases/netperfbenchmark/pkg/controller"
+	"k8s.io/perf-tests/util-images/phases/netperfbenchmark/pkg/worker"
+)
+
+var (
+	mode         = flag.String("mode", "", "Mode that should be run. Supported values: controller or worker")
+	ratio        = flag.String("client-server-pod-ratio", "", "Client POD to Server POD ratio")
+	duration     = flag.String("measurement-duration", "", "Duration of metric collection in seconds")
+	protocol     = flag.String("protocol", "", "Protocol to be tested. Supported values: tcp or or udp or http")
+	controllerIp = flag.String("controllerIP", "", "IP address of controller pod")
+)
+
+func main() {
+	klog.InitFlags(flag.CommandLine)
+	flag.Parse()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	klog.Infof("Pod running in: %s mode %s ratio\n", *mode, *ratio)
+
+	err := validate(*mode, *ratio, *protocol, *duration)
+	if err != nil {
+		klog.Fatalf("Validation failed with err : %s", err)
+	}
+
+	switch *mode {
+	case api.ControllerMode:
+		controller.Start(*ratio)
+		controller.WaitForWorkerPodReg()
+		controller.ExecuteTest(*ratio, *duration, *protocol)
+	case api.WorkerMode:
+		worker.Start(*controllerIp)
+	default:
+		klog.Fatalf("Unrecognized mode: %q", *mode)
+	}
+
+	wg.Wait()
+}
+
+func validate(mode string, ratio string, protocol string, duration string) error {
+
+	if mode != api.ControllerMode && mode != api.WorkerMode {
+		return errors.New("invalid mode")
+	}
+
+	if mode == api.WorkerMode && *controllerIp == "" {
+		return errors.New("Controller hostname/ip not specified")
+	} else if mode == api.WorkerMode {
+		return nil
+	}
+
+	if mode == api.ControllerMode && duration == "" {
+		return errors.New("Duration not specified. Mandatory param for controller-mode")
+	}
+	if !strings.Contains(ratio, api.RatioSeparator) {
+		return errors.New("invalid ratio. : missing")
+	}
+
+	if protocol != api.Protocol_TCP && protocol != api.Protocol_UDP && protocol != api.Protocol_HTTP {
+		return errors.New("invalid protocol")
+	}
+	return nil
+}

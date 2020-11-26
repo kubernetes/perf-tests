224f6dcb757f68b793825c6f6a38d5a201fc2942
Patrick Ohly <patrick.ohly@intel.com>
2020-11-26 13:33:08 +0100

    WIP: initial ad-hod scale testing

--- adhoc/run-e2e-test.sh
+++ adhoc/run-e2e-test.sh
@@ -18,6 +18,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+set -x
+
 # The KUBECONFIG env variable will point to a valid kubeconfig giving you an
 # admin acess to a fresh 100 node cluster. You don't have to tear the cluster
 # down, it will happen automatically after the test.
@@ -28,8 +30,34 @@ export KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
 # automatically uploaded to gcs after the test completion (successful or not).
 export ARTIFACTS_DIR=/workspace/_artifacts
 
+# KUBECTL is already set, but to a relative path (./cluster/kubectl.sh).
+# Just use the normal command in PATH.
+export KUBECTL=kubectl
 
-# Implement ad-hoc test here, e.g. install extra addons via kubectl then run
-# clusterloader2 with your custom config.
+# Check out the right branch of PMEM-CSI with support for the ad-hoc scale testing.
+git clone --branch scale-testing https://github.com/pohly/pmem-csi.git
+cd pmem-csi
+
+# Reconfigure for 100 node cluster and job environment.
+# TODO: let the script accept env vars...
+sed -i \
+    -e 's;^expected_rate=.*;expected_rate=50;' \
+    -e 's;^expected_duration=.*;expected_duration=60;' \
+    -e "s;^result_dir=.*;result_dir=${ARTIFACTS_DIR}/scale-test;" \
+    -e 's;^GATHER_METRICS: false;GATHER_METRICS: true;' \
+    -e 's;--provider=local;--provider=gce;' \
+    hack/scale-test.sh
+
+# Install pod vertical autoscaler.
+hack/setup-va.sh
+
+# Label all scheduleable nodes with storage=pmem.
+# https://stackoverflow.com/questions/41348531/how-to-identify-schedulable-nodes-in-kubernetes/58275106#58275106
+for node in $(kubectl get no -o 'go-template={{range .items}}{{$taints:=""}}{{range .spec.taints}}{{if eq .effect "NoSchedule"}}{{$taints = print $taints .key ","}}{{end}}{{end}}{{if not $taints}}{{.metadata.name}}{{ "\n"}}{{end}}{{end}}'); do
+    kubectl label node/$node storage=pmem
+done
+
+# Ready..
+hack/scale-test.sh
 
 exit 0

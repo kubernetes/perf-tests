0793ffdb0b0d8a09b16fd5c9010ae9b8e5c5591f
Weilong Cui <cuiwl@google.com>
2021-01-26 14:36:12 -0800

    Enable Anet scraping
    Signed-off-by: Weilong Cui <cuiwl@google.com>

--- clusterloader2/pkg/config/cluster.go
+++ clusterloader2/pkg/config/cluster.go
@@ -72,6 +72,7 @@ type PrometheusConfig struct {
 	ScrapeNodeExporter      bool
 	ScrapeKubelets          bool
 	ScrapeKubeProxy         bool
+	ScrapeAnet              bool
 	SnapshotProject         string
 	ManifestPath            string
 	CoreManifests           string

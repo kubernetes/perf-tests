b2f1880fc0b951646733f00f9e219eca6490a259
Braulio.Dumba <brauliodumba@gmail.com>
2021-12-02 11:02:43 -0500

    Adding Poisson tunningset load generator

---
+++ clusterloader2/pkg/tuningset/poisson_load.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tuningset
+
+import (
+	"math/rand"
+	"time"
+	"math"
+
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/perf-tests/clusterloader2/api"
+)
+
+type poissonLoad struct {
+	params *api.PoissonLoad
+}
+
+func newPoissonLoad(params *api.PoissonLoad) TuningSet {
+	return &poissonLoad{
+		params: params,
+	}
+}
+
+func (rl *poissonLoad) Execute(actions []func()) {
+	var wg wait.Group
+	for i := range actions {
+		wg.Start(actions[i])
+		time.Sleep(interArrivalTimes(rl.params.RatePS))
+	}
+	wg.Wait()
+}
+
+//Simulating inter-arrival times in a Poisson process
+func interArrivalTimes(RatePS float64) time.Duration {
+	p := rand.Float64()
+
+	return time.Duration(int(float64(time.Second) * (math.Log(1 - p)/RatePS)))
+}

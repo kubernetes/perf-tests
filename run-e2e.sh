306f1b697fd5c17fdd5d6193f1104686f80b60fc
jupblb <jupblb@google.com>
2022-03-23 17:12:32 +0100

    Run command from the first word of the first arg in run-e2e.sh
    run-e2e.sh is usually ran from kubetest (test-infra). It is defined by
    repeatedly declaring test-cmd-args argument, which is itself
    a leftover after using pflag dependency. It may be wanted to use only
    a single declaration of this argument. In such a case, the first
    argument will contain all the parameters for ClusterLoader2.
    
    This change makes the approach above possible by setting the tool name
    to the first word of the first argument value and adds the remainder
    as additional flags for the clusterloader2/run-e2e.sh command.

--- run-e2e.sh
+++ run-e2e.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,8 +16,9 @@
 set -o nounset
 set -o pipefail
 
-PERFTEST_ROOT=$(dirname "${BASH_SOURCE}")
-echo "TOOL_NAME: $1"
+PERFTEST_ROOT=$(dirname "${BASH_SOURCE[0]}")
+TOOL_NAME=$(echo "$1" | cut -d " " -f1)
+echo "TOOL_NAME: $TOOL_NAME"
 
 # TODO(kubernetes/perf-tests/issues/1624): Get rid of the logic below.
 # Print the git history. This is useful in the CI jobs as it provides an ability
@@ -25,25 +26,26 @@ echo "TOOL_NAME: $1"
 if [[ "${PERF_TESTS_PRINT_COMMIT_HISTORY:-false}" == "true" ]]; then
   # The output file location assumes the script works inside a prow job.
   output_file="${ARTIFACTS}/perf-tests.gitlog"
-  echo "k8s.io/perf-tests git log:" | tee $output_file || true
-  git -C $PERFTEST_ROOT log -n 10 --format="%H - %ad (%s)" --date=local | tee -a $output_file || true
+  echo "k8s.io/perf-tests git log:" | tee "$output_file" || true
+  git -C "$PERFTEST_ROOT" log -n 10 --format="%H - %ad (%s)" --date=local | tee -a "$output_file" || true
 fi
 
-case "$1" in
+case "$TOOL_NAME" in
   cluster-loader2 )
     #CLUSTERLOADER2
-    echo "COMMAND: ${PERFTEST_ROOT}/clusterloader2 && ./run-e2e.sh ${@:2}"
-    cd ${PERFTEST_ROOT}/clusterloader2 && ./run-e2e.sh ${@:2}
+    IFS=' ' read -ra args_1 <<< "$1"
+    echo "COMMAND: ${PERFTEST_ROOT}/clusterloader2 && ./run-e2e.sh ${args_1[*]:1} ${*:2}"
+    cd "${PERFTEST_ROOT}/clusterloader2" && exec ./run-e2e.sh "${args_1[@]:1}" "${@:2}"
     exit
     ;;
   network-performance )
     #NETPERF
-    cd ${PERFTEST_ROOT}/network/benchmarks/netperf/ && go run ./launch.go  --kubeConfig="${HOME}/.kube/config" --hostnetworking --iterations 1
+    cd "${PERFTEST_ROOT}/network/benchmarks/netperf/" && go run ./launch.go  --kubeConfig="${HOME}/.kube/config" --hostnetworking --iterations 1
     exit
     ;;
   kube-dns|core-dns|node-local-dns )
-    cd ${PERFTEST_ROOT}/dns
-    ./run $@
+    cd "${PERFTEST_ROOT}/dns" || exit
+    ./run "$@"
     exit
     ;;
   --help | -h )

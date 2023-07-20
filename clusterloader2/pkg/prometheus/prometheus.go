6a4903209fae9eff2081b872f9ef4df8efbacce9
Alexander Cristurean <36670455+cristure@users.noreply.github.com>
2023-07-20 09:23:00 +0300

    Update prometheus.go

--- clusterloader2/pkg/prometheus/prometheus.go
+++ clusterloader2/pkg/prometheus/prometheus.go
@@ -35,7 +35,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
-
 	"k8s.io/perf-tests/clusterloader2/pkg/config"
 	clerrors "k8s.io/perf-tests/clusterloader2/pkg/errors"
 	"k8s.io/perf-tests/clusterloader2/pkg/flags"

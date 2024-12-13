abf58be7aeee14a402fe27603b45b5cbf45cb1f5
Wojciech Tyczyński <wojtekt@google.com>
2024-12-13 11:48:23 +0100

    Fix clusterloader2 failures after flag registration changes

--- clusterloader2/run-e2e.sh
+++ clusterloader2/run-e2e.sh
@@ -131,4 +131,4 @@ users:
 export KUBECONFIG=${kubeconfig}
 
 cd "${CLUSTERLOADER_ROOT}"/ && go build -o clusterloader './cmd/'
-./clusterloader --alsologtostderr --v="${CL2_VERBOSITY:-2}" "$@"
+./clusterloader --v="${CL2_VERBOSITY:-2}" "$@"

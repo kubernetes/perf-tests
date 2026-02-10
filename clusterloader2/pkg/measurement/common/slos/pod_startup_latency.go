9168d057ee86d288ccc86723119c65c2996e1aae
Yahia Naguib <yahian@google.com>
2026-02-10 14:08:43 +0000

    Refactor: move event filtering inside mapEventsByOrder block

--- clusterloader2/pkg/measurement/common/slos/pod_startup_latency.go
+++ clusterloader2/pkg/measurement/common/slos/pod_startup_latency.go
@@ -304,17 +304,17 @@ func (p *podStartupLatencyMeasurement) gatherScheduleTimes(c clientset.Interface
 		return err
 	}
 
-	// Filter events to only include those that belong to pods we are tracking.
-	var filteredEvents []corev1.Event
-	for _, event := range schedEvents.Items {
-		key := createMetaNamespaceKey(event.InvolvedObject.Namespace, event.InvolvedObject.Name)
-		if _, exists := p.podStartupEntries.Get(key, createPhase); exists {
-			filteredEvents = append(filteredEvents, event)
-		}
-	}
-	schedEvents.Items = filteredEvents
-
 	if p.mapEventsByOrder {
+		// Filter events to only include those that belong to pods we are tracking.
+		var filteredEvents []corev1.Event
+		for _, event := range schedEvents.Items {
+			key := createMetaNamespaceKey(event.InvolvedObject.Namespace, event.InvolvedObject.Name)
+			if _, exists := p.podStartupEntries.Get(key, createPhase); exists {
+				filteredEvents = append(filteredEvents, event)
+			}
+		}
+		schedEvents.Items = filteredEvents
+
 		orderedCreates := p.podStartupEntries.GetOrderedKeys(createPhase)
 		if len(orderedCreates) != len(schedEvents.Items) {
 			return fmt.Errorf("number of pod creations (%d) does not match number of scheduling events gathered (%d)", len(orderedCreates), len(schedEvents.Items))

4d4f65626870902101b3cca4ca2be80a1eea7454
Franciszek <fbalcera@google.com>
2026-08-27 09:43:19 +0000

    Fix lint

--- clusterloader2/pkg/measurement/util/shared_node_indexer.go
+++ clusterloader2/pkg/measurement/util/shared_node_indexer.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2026 The Kubernetes Authors.
+Copyright The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -22,8 +22,8 @@ import (
 	"sync"
 	"time"
 
-	coreinformers "k8s.io/client-go/informers/core/v1"
 	"k8s.io/client-go/informers"
+	coreinformers "k8s.io/client-go/informers/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/perf-tests/clusterloader2/pkg/measurement/util/informer"
